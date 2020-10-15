8a3970112a9078ea789c9cd8270319c408854201
leandro <leandrojc88@gmail.com>
2020-10-15 16:56:11 -0400

    Inicio Ajuste Salida

---
+++ src/Entity/Contabilidad/Inventario/Expediente.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity\Contabilidad\Inventario;
+
+use App\Entity\Contabilidad\Config\Unidad;
+use App\Repository\Contabilidad\Inventario\ExpedienteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ExpedienteRepository::class)
+ */
+class Expediente
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $codigo;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $descripcion;
+
+    /**
+     * @ORM\OneToOne(targetEntity=Unidad::class, cascade={"persist", "remove"})
+     */
+    private $id_unidad;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCodigo(): ?string
+    {
+        return $this->codigo;
+    }
+
+    public function setCodigo(string $codigo): self
+    {
+        $this->codigo = $codigo;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): self
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    public function getIdUnidad(): ?Unidad
+    {
+        return $this->id_unidad;
+    }
+
+    public function setIdUnidad(?Unidad $id_unidad): self
+    {
+        $this->id_unidad = $id_unidad;
+
+        return $this;
+    }
+}
--- src/Entity/Contabilidad/Inventario/MovimientoMercancia.php
+++ src/Entity/Contabilidad/Inventario/MovimientoMercancia.php
@@ -90,6 +90,11 @@ class MovimientoMercancia
      */
     private $id_almacen;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Expediente::class, nullable="true")
+     */
+    private $id_expediente;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -247,6 +252,18 @@ class MovimientoMercancia
     public function setIdAlmacen(?Almacen $id_almacen): self
     {
         $this->id_almacen = $id_almacen;
+
+        return $this;
+    }
+
+    public function getIdExpediente(): ?Expediente
+    {
+        return $this->id_expediente;
+    }
+
+    public function setIdExpediente(?Expediente $id_expediente): self
+    {
+        $this->id_expediente = $id_expediente;
 
         return $this;
     }
