f9f8a77ef5a31717f3894662edee5c79d5cc6aa2
leandro <leandrojc88@gmail.com>
2020-10-20 08:34:26 -0400

    revision y correccion de errores en las funcionalidades

--- migrations/Version20201020005651.php
+++ migrations/Version20201020005651.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201019185903 extends AbstractMigration
+final class Version20201020005651 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,14 +20,12 @@ final class Version20201019185903 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE expediente ADD anno INT NOT NULL');
-        $this->addSql('ALTER TABLE orden_trabajo ADD anno INT NOT NULL');
+        $this->addSql('ALTER TABLE expediente CHANGE id_unidad_id id_unidad_id INT DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE expediente DROP anno');
-        $this->addSql('ALTER TABLE orden_trabajo DROP anno');
+        $this->addSql('ALTER TABLE expediente CHANGE id_unidad_id id_unidad_id INT NOT NULL');
     }
 }
--- src/Controller/Contabilidad/General/RegistroComprobantesController.php
+++ src/Controller/Contabilidad/General/RegistroComprobantesController.php
@@ -14,46 +14,87 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * Class RegistroComprobantesController
  * @package App\Controller\Contabilidad\General
- * @Route("/contabilidad/general/registro/comprobantes")
+ * @Route("/contabilidad/general/registro-comprobantes")
  */
 class RegistroComprobantesController extends AbstractController
 {
     /**
      * @Route("/", name="contabilidad_general_registro_comprobantes")
      */
-    public function index(EntityManagerInterface $em,Request $request)
+    public function index(EntityManagerInterface $em, Request $request)
+    {
+
+        return $this->render('contabilidad/general/registro_comprobantes/index.html.twig', [
+            'controller_name' => 'RegistroComprobantesController',
+            'registro' => $this->getData($em, $request)
+        ]);
+    }
+
+    public function getData(EntityManagerInterface $em, Request $request)
     {
         /** @var User $user */
         $user = $this->getUser();
         /** @var Empleado $empleado */
         $empleado = $em->getRepository(Empleado::class)->findOneBy(array(
-            'id_usuario'=>$user
+            'id_usuario' => $user
         ));
         $row = [];
-        if($empleado){
+        if ($empleado) {
             /** @var Unidad $obj_unidad */
             $obj_unidad = $empleado->getIdUnidad();
             $comprobantes = $em->getRepository(RegistroComprobantes::class)->findBy(array(
-                'anno'=>Date('Y'),
-                'id_unidad'=>$obj_unidad
+                'anno' => Date('Y'),
+                'id_unidad' => $obj_unidad
             ));
             /** @var RegistroComprobantes $comp */
-            foreach ($comprobantes as $comp){
+            foreach ($comprobantes as $comp) {
+                $obj_empleado = $em->getRepository(Empleado::class)->findOneBy(array(
+                    'activo' => true,
+                    'id_usuario' => $comp->getIdUsuario()->getId()
+                ));
                 $row[] = array(
-                    'id'=>$comp->getId(),
-                    'nro'=>$comp->getNroConsecutivo(),
-                    'tipo_comprobante'=>$comp->getIdTipoComprobante()->getDescripcion(),
-                    'abreviatura_comprobante'=>$comp->getIdTipoComprobante()->getAbreviatura(),
-                    'descripcion'=>$comp->getDescripcion(),
-                    'fecha'=>$comp->getFecha()->format('d-m-Y'),
-                    'usuario'=>$comp->getIdUsuario()->getUsername(),
-                    'almacen'=>$comp->getIdAlmacen()->getCodigo().'-'.$comp->getIdAlmacen()->getDescripcion()
+                    'id' => $comp->getId(),
+                    'nro' => $comp->getNroConsecutivo(),
+                    'tipo_comprobante' => $comp->getIdTipoComprobante()->getDescripcion(),
+                    'abreviatura_comprobante' => $comp->getIdTipoComprobante()->getAbreviatura(),
+                    'descripcion' => $comp->getDescripcion(),
+                    'fecha' => $comp->getFecha()->format('d-m-Y'),
+                    'usuario' => $obj_empleado->getNombre(),
+                    'almacen' => $comp->getIdAlmacen()->getCodigo() . '-' . $comp->getIdAlmacen()->getDescripcion()
                 );
             }
         }
-        return $this->render('contabilidad/general/registro_comprobantes/index.html.twig', [
-            'controller_name' => 'RegistroComprobantesController',
-            'registro'=>$row
+        return $row;
+    }
+
+    /**
+     * @param EntityManagerInterface $em
+     * @param Request $request
+     * @param $id
+     * @Route("/print_registro", name="contabilidad_general_registro_comprobantes_print")
+     */
+    public function print(EntityManagerInterface $em, Request $request)
+    {
+        $id_user = $this->getUser()->getId();
+        $obj_empleado = $em->getRepository(Empleado::class)->findOneBy(array(
+            'activo' => true,
+            'id_usuario' => $id_user
+        ));
+        $codigo = $obj_empleado->getIdUnidad()->getCodigo();
+        $nombre = $obj_empleado->getIdUnidad()->getNombre();
+        return $this->render('contabilidad/general/registro_comprobantes/print.html.twig', [
+            'datos' => $this->getData($em, $request),
+            'unidad_nombre'=>$nombre,
+            'unidad_codigo'=>$codigo
         ]);
+    }
+
+    /**
+     * @param EntityManagerInterface $em
+     * @param Request $request
+     * @Route("/detalles/{$id}", name="contabilidad_general_registro_comprobantes_detalles")
+     */
+    public function getDetalles(EntityManagerInterface $em, Request $request,$id){
+        dd($id);
     }
 }
