f33c77c97683cafd2baa010bedb69a78a64da905
TI <programacion.grupohorizontes@gmail.com>
2020-11-05 16:01:06 -0400

    subiendo cambios

--- src/Controller/FacturaController.php
+++ src/Controller/FacturaController.php
@@ -606,8 +606,8 @@ class FacturaController extends AbstractController
         $filename = 'factura.pdf';
 
        
-
-             //return $this->render('factura/cash.html.twig',['json' => \json_decode($cotizacion->getJson())]);
+                
+             return $this->render('factura/cash.html.twig',['cambio' => $efectivo[0] , 'moneda' =>  $factura->getIdMoneda(),"total" => number_format ( $factura->getTotal(),2,",", " " ), "empleado" => $factura->getEmpleado(),"cliente" => $clienteOrigen[0],"json" => $json,"id" => $factura->getId(),"nombre" => $factura->getNombreCliente(),"telefono" => $factura->getIdCliente()]);
         return new PdfResponse($pdf->getOutputFromHtml($html),$filename,'application/pdf',"inline");
     }
 
