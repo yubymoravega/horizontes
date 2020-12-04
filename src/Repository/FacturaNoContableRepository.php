5688da54f8001afa7f9b9477e211d4367bf2bdf3
TI <programacion.grupohorizontes@gmail.com>
2020-12-04 16:12:31 -0400

    factura no contable

--- src/Repository/FacturaNoContableRepository.php
+++ src/Repository/FacturaNoContableRepository.php
@@ -16,7 +16,7 @@ class FacturaNoContableRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Factura::class);
+        parent::__construct($registry, FacturaNoContable::class);
     }
 
     // /**
