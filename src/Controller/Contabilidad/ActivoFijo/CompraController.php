c356b6c34a66e003783b0cd57703c5421d28eebe
Leandro Capdesuñer García <leandrojc88@gmail.com>
2021-01-11 11:58:59 -0500

    Factura y activos fijos fixed load

--- src/Controller/Contabilidad/ActivoFijo/CompraController.php
+++ src/Controller/Contabilidad/ActivoFijo/CompraController.php
@@ -39,7 +39,10 @@ class CompraController extends AbstractController
             $movimiento_compra = $request->get('movimiento_activo_fijo');
             $nro_inventatio = $movimiento_compra['nro_inventatio'];
             $fundamentacion = $movimiento_compra['fundamentacion'];
+            $fecha_factura = $movimiento_compra['fecha_factura'];
+            $nro_factura = $movimiento_compra['nro_factura'];
             $id_proveedor = $movimiento_compra['id_proveedor'];
+//            dd($movimiento_compra);
             $obj_proveedor = $em->getRepository(Proveedor::class)->find($id_proveedor);
             $user = $this->getUser();
             $obj_unidad = AuxFunctions::getUnidad($em, $user);
@@ -87,7 +90,8 @@ class CompraController extends AbstractController
                 ->setEntrada(true)
                 ->setFundamentacion($fundamentacion)
                 ->setIdProveedor($obj_proveedor)
-            ;
+                ->setFechaFactura(\DateTime::createFromFormat('Y-m-d', $fecha_factura))
+                ->setNroFactura($nro_factura);
             $em->persist($new_movimiento);
 
             $obj_activo->setActivo(true);
@@ -97,22 +101,22 @@ class CompraController extends AbstractController
                 $asiento_activo = AuxFunctions::createAsiento($em, $cuentas_activo_fijo->getIdCuentaActivo(), $cuentas_activo_fijo->getIdSubcuentaActivo(), null,
                     $obj_activo->getIdUnidad(), null, $cuentas_activo_fijo->getIdCentroCostoActivo(), null, null, null, null, 0, 0,
                     $obj_activo->getFechaAlta(), $obj_activo->getFechaAlta()->format('Y'), 0, $obj_activo->getValorInicial(),
-                    $new_movimiento->getIdTipoMovimiento()->getCodigo() . '-' . $new_movimiento->getNroConsecutivo(), null,$obj_activo,$cuentas_activo_fijo->getIdAreaResponsabilidadActivo());
+                    $new_movimiento->getIdTipoMovimiento()->getCodigo() . '-' . $new_movimiento->getNroConsecutivo(), null, $obj_activo, $cuentas_activo_fijo->getIdAreaResponsabilidadActivo());
                 //asentando la cuenta de depresicion del activo
                 if ($obj_activo->getDepreciacionAcumulada() > 0) {
                     $asiento_depresiacion = AuxFunctions::createAsiento($em, $cuentas_activo_fijo->getIdCuentaDepreciacion(), $cuentas_activo_fijo->getIdSubcuentaDepreciacion(), null,
                         $obj_activo->getIdUnidad(), null, null, null, null, null, null, 0, 0,
                         $obj_activo->getFechaAlta(), $obj_activo->getFechaAlta()->format('Y'), $obj_activo->getDepreciacionAcumulada(), 0,
-                        $new_movimiento->getIdTipoMovimiento()->getCodigo() . '-' . $new_movimiento->getNroConsecutivo(), null,$obj_activo);
+                        $new_movimiento->getIdTipoMovimiento()->getCodigo() . '-' . $new_movimiento->getNroConsecutivo(), null, $obj_activo);
                 }
 
                 //asentando la cuenta de acreedora del activo(600)
-                $cuenta_acreedora = $em->getRepository(Cuenta::class)->findOneBy(['nro_cuenta'=>'600','activo'=>true]);
-                $subcuenta_acreedora = $em->getRepository(Subcuenta::class)->findOneBy(['id_cuenta'=>$cuenta_acreedora,'nro_subcuenta'=>'0010','activo'=>true]);
+                $cuenta_acreedora = $em->getRepository(Cuenta::class)->findOneBy(['nro_cuenta' => '600', 'activo' => true]);
+                $subcuenta_acreedora = $em->getRepository(Subcuenta::class)->findOneBy(['id_cuenta' => $cuenta_acreedora, 'nro_subcuenta' => '0010', 'activo' => true]);
                 $asiento_acreedora = AuxFunctions::createAsiento($em, $cuenta_acreedora, $subcuenta_acreedora, null,
                     $obj_activo->getIdUnidad(), null, $cuentas_activo_fijo->getIdCentroCostoGasto(), $cuentas_activo_fijo->getIdElementoGastoGasto(), null, null, null, 0, 0,
                     $obj_activo->getFechaAlta(), $obj_activo->getFechaAlta()->format('Y'), $obj_activo->getValorReal(), 0,
-                    $new_movimiento->getIdTipoMovimiento()->getCodigo() . '-' . $new_movimiento->getNroConsecutivo(), null,$obj_activo);
+                    $new_movimiento->getIdTipoMovimiento()->getCodigo() . '-' . $new_movimiento->getNroConsecutivo(), null, $obj_activo);
 
                 //actualizando la cuenta acreedora del activo fijo
                 $cuentas_activo_fijo
@@ -121,20 +125,20 @@ class CompraController extends AbstractController
                 $em->persist($cuentas_activo_fijo);
 
                 //asentando cuenta que disminuye la reserva para invensiones(646)
-                $cuenta_inversiones = $em->getRepository(Cuenta::class)->findOneBy(['nro_cuenta'=>'646','activo'=>true]);
-                $subcuenta_inversiones = $em->getRepository(Subcuenta::class)->findOneBy(['id_cuenta'=>$cuenta_acreedora,'nro_subcuenta'=>'0010','activo'=>true]);
+                $cuenta_inversiones = $em->getRepository(Cuenta::class)->findOneBy(['nro_cuenta' => '646', 'activo' => true]);
+                $subcuenta_inversiones = $em->getRepository(Subcuenta::class)->findOneBy(['id_cuenta' => $cuenta_acreedora, 'nro_subcuenta' => '0010', 'activo' => true]);
                 $asiento_inversiones = AuxFunctions::createAsiento($em, $cuenta_inversiones, $subcuenta_inversiones, null,
                     $obj_activo->getIdUnidad(), null, $cuentas_activo_fijo->getIdCentroCostoGasto(), $cuentas_activo_fijo->getIdElementoGastoGasto(), null, null, null, 0, 0,
                     $obj_activo->getFechaAlta(), $obj_activo->getFechaAlta()->format('Y'), 0, $obj_activo->getValorReal(),
-                    $new_movimiento->getIdTipoMovimiento()->getCodigo() . '-' . $new_movimiento->getNroConsecutivo(), null,$obj_activo);
+                    $new_movimiento->getIdTipoMovimiento()->getCodigo() . '-' . $new_movimiento->getNroConsecutivo(), null, $obj_activo);
 
                 //Asentando la cuenta por pagar de activo fijo que aumenta por el credito(421)
-                $cuenta_por_pagar = $em->getRepository(Cuenta::class)->findOneBy(['nro_cuenta'=>'421','activo'=>true]);
-                $subcuenta_por_pagar = $em->getRepository(Subcuenta::class)->findOneBy(['id_cuenta'=>$cuenta_acreedora,'nro_subcuenta'=>'0010','activo'=>true]);
+                $cuenta_por_pagar = $em->getRepository(Cuenta::class)->findOneBy(['nro_cuenta' => '421', 'activo' => true]);
+                $subcuenta_por_pagar = $em->getRepository(Subcuenta::class)->findOneBy(['id_cuenta' => $cuenta_acreedora, 'nro_subcuenta' => '0010', 'activo' => true]);
                 $asiento_por_pagar = AuxFunctions::createAsiento($em, $cuenta_por_pagar, $subcuenta_por_pagar, null,
                     $obj_activo->getIdUnidad(), null, $cuentas_activo_fijo->getIdCentroCostoGasto(), $cuentas_activo_fijo->getIdElementoGastoGasto(), null, null, $obj_proveedor, 0, 0,
-                    $obj_activo->getFechaAlta(), $obj_activo->getFechaAlta()->format('Y'), $obj_activo->getValorReal(),0,
-                    $new_movimiento->getIdTipoMovimiento()->getCodigo() . '-' . $new_movimiento->getNroConsecutivo(), null,$obj_activo);
+                    $obj_activo->getFechaAlta(), $obj_activo->getFechaAlta()->format('Y'), $obj_activo->getValorReal(), 0,
+                    $new_movimiento->getIdTipoMovimiento()->getCodigo() . '-' . $new_movimiento->getNroConsecutivo(), null, $obj_activo);
 
 
                 $em->flush();
@@ -172,18 +176,18 @@ class CompraController extends AbstractController
         $tipo_movimiento = $em->getRepository(TipoMovimiento::class)->find($this->tipo_movimiento);
         $unidad = AuxFunctions::getUnidad($em, $this->getUser());
         $proveedores = [];
-        $data_proveedores = $em->getRepository(Proveedor::class)->findBy(['activo'=>true]);
+        $data_proveedores = $em->getRepository(Proveedor::class)->findBy(['activo' => true]);
         /** @var Proveedor $proveedor */
-        foreach ($data_proveedores as $proveedor){
+        foreach ($data_proveedores as $proveedor) {
             $proveedores[] = [
-                'id'=>$proveedor->getId(),
-                'nombre'=>$proveedor->getCodigo().' - '.$proveedor->getNombre()
+                'id' => $proveedor->getId(),
+                'nombre' => $proveedor->getCodigo() . ' - ' . $proveedor->getNombre()
             ];
         }
         return new JsonResponse([
             'cuentas' => $row,
             'success' => true,
-            'proveedores'=>$proveedores,
+            'proveedores' => $proveedores,
             'nros' => AuxFunctions::getConsecutivoActivoFijo($em, $tipo_movimiento, $unidad, AuxFunctions::getCurrentYear($em, $unidad))
         ]);
     }
@@ -234,12 +238,14 @@ class CompraController extends AbstractController
         /** @var ActivoFijoCuentas $cuenta_activo */
         $cuenta_activo = $em->getRepository(ActivoFijoCuentas::class)->findOneBy(['id_activo' => $obj_movimiento_activo_fijo->getIdActivoFijo()]);
         $row = array(
-            'id_proveedor'=>$obj_movimiento_activo_fijo->getIdProveedor()?$obj_movimiento_activo_fijo->getIdProveedor()->getId():'',
-            'cancelado'=>$obj_movimiento_activo_fijo->getCancelado(),
+            'id_proveedor' => $obj_movimiento_activo_fijo->getIdProveedor() ? $obj_movimiento_activo_fijo->getIdProveedor()->getId() : '',
+            'cancelado' => $obj_movimiento_activo_fijo->getCancelado(),
             'nro_inv' => $obj_movimiento_activo_fijo->getIdActivoFijo()->getNroInventario(),
             'desc' => $obj_movimiento_activo_fijo->getIdActivoFijo()->getDescripcion(),
             'fecha' => $obj_movimiento_activo_fijo->getFecha()->format('d/m/Y'),
             'fundamentacion' => $obj_movimiento_activo_fijo->getFundamentacion(),
+            'fecha_factura' => $obj_movimiento_activo_fijo->getFechaFactura()->format('d/m/Y'),
+            'nro_factura' => $obj_movimiento_activo_fijo->getNroFactura(),
             'nro_cuenta' => $obj_movimiento_activo_fijo->getIdCuenta()->getNroCuenta() . ' - ' . $obj_movimiento_activo_fijo->getIdCuenta()->getNombre(),
             'nro_subcuenta' => $obj_movimiento_activo_fijo->getIdSubcuenta()->getNroSubcuenta() . ' - ' . $obj_movimiento_activo_fijo->getIdSubcuenta()->getDescripcion(),
             'centro_costo' => $cuenta_activo->getIdCentroCostoActivo()->getCodigo() . ' - ' . $cuenta_activo->getIdCentroCostoActivo()->getNombre(),
