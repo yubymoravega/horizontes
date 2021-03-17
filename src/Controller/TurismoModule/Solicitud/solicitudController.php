59dbdd000b2020a35e326b5fb8498c7702efe1be
juniorag <46666399+juniorag89@users.noreply.github.com>
2021-03-16 22:38:45 -0400

    Persistencia de los datos de tranfer, tour y rentcar
    Ajustes en validaciones

--- src/Controller/TurismoModule/Solicitud/solicitudController.php
+++ src/Controller/TurismoModule/Solicitud/solicitudController.php
@@ -8,6 +8,7 @@ use App\Entity\TurismoModule\Solicitud\SolRentCar;
 use App\Entity\TurismoModule\Solicitud\SolTour;
 use App\Entity\TurismoModule\Solicitud\SolTranfer;
 use App\Entity\TurismoModule\Solicitud\SolVuelo;
+use App\Entity\TurismoModule\Tour\Tour;
 use App\Entity\TurismoModule\Traslado\Lugares;
 use App\Entity\TurismoModule\Traslado\TipoVehiculo;
 use App\Form\TurismoModule\Solicitud\SolHotelType;
@@ -16,7 +17,6 @@ use App\Form\TurismoModule\Solicitud\SolTourType;
 use App\Form\TurismoModule\Solicitud\SolTranferType;
 use App\Form\TurismoModule\Solicitud\SolVueloType;
 use Doctrine\ORM\EntityManagerInterface;
-use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -137,6 +137,99 @@ class solicitudController extends AbstractController
                 return new \Exception('La petición ha retornado un error, contacte a su proveedor de software.');
             }
         }
+        //Si el form tranfer esta completo
+        if($request->get('tranfer') == 'true'){
+            $viaje_tranfer = false;
+            $cant_adulto_tranfer = $request->get('cant_adulto_tranfer');
+            $cant_nino_tranfer = $request->get('cant_nino_tranfer');
+            $fecha_tranfer = \DateTime::createFromFormat('m/d/Y h:i A',$request->get('fecha_tranfer'));
+            $fechaS_tranfer = \DateTime::createFromFormat('m/d/Y h:i A',$request->get('fechas_tranfer'));
+            $origen_tranfer = $em->getRepository(Lugares::class)->find($request->get('origen_tranfer'));
+            $destino_tranfer = $em->getRepository(Lugares::class)->find($request->get('destino_tranfer'));
+            $tipoVehiculo_tranfer = $em->getRepository(TipoVehiculo::class)->find($request->get('tipoVehiculo_tranfer'));
+            if($request->get('viaje_tranfer') == 'true'){
+                $viaje_tranfer = true;
+            }
+            $comentario_tranfer = $request->get('comentario_tranfer');
+
+            $obj_tranfer = new SolTranfer();
+            $obj_tranfer
+                ->setCantAdulto($cant_adulto_tranfer)
+                ->setCantNino($cant_nino_tranfer)
+                ->setFecha($fecha_tranfer)
+                ->setFechaSalida($fechaS_tranfer)
+                ->setOrigen($origen_tranfer)
+                ->setDestino($destino_tranfer)
+                ->setTipoVehiculo($tipoVehiculo_tranfer)
+                ->setIdaRetorno($viaje_tranfer)
+                ->setComentario($comentario_tranfer)
+                ->setActivo(true);
+            try {
+                $em->persist($obj_tranfer);
+                $em->flush();
+            }catch (FileException $exception) {
+                $error = true;
+                return new \Exception('La petición ha retornado un error, contacte a su proveedor de software.');
+            }
+        }
+
+        //Si el form tour esta completo
+        if($request->get('tour') == 'true'){
+            $nombre_tour = $em->getRepository(Tour::class)->find($request->get('nombre_tour'));
+            $cant_adulto_tour = $request->get('cant_adulto_tour');
+            $cant_nino_tour = $request->get('cant_nino_tour');
+            $fecha_tour = \DateTime::createFromFormat('m/d/Y h:i A',$request->get('fecha_tour'));
+            $comentario_tour = $request->get('comentario_tour');
+
+            $obj_tour = new SolTour();
+            $obj_tour
+                ->setTour($nombre_tour)
+                ->setCantNino($cant_nino_tour)
+                ->setCantAdulto($cant_adulto_tour)
+                ->setFechaSalida($fecha_tour)
+                ->setComentario($comentario_tour)
+                ->setActivo(true);
+
+            try {
+                $em->persist($obj_tour);
+                $em->flush();
+            }catch (FileException $exception) {
+                $error = true;
+                return new \Exception('La petición ha retornado un error, contacte a su proveedor de software.');
+            }
+        }
+
+        //Si el form rentcar esta completo
+        if($request->get('rentcar') == 'true'){
+            $cant_personas_rentcar = $request->get('cant_persona_rentcar');
+            $tipoVehiculo_rentcar = $em->getRepository(TipoVehiculo::class)->find($request->get('tipoVehiculo_rentcar'));
+            $recogida_rentcar = $em->getRepository(Lugares::class)->find($request->get('recogida_rentcar'));
+            $entrega_rentcar = $em->getRepository(Lugares::class)->find($request->get('entrega_rentcar'));
+            $fechad_rentcar = \DateTime::createFromFormat('m/d/Y h:i A',$request->get('fechad_rentcar'));
+            $fechah_rentcar = \DateTime::createFromFormat('m/d/Y h:i A',$request->get('fechah_rentcar'));
+            $comentario_rentcar = $request->get('comentario_rentcar');
+
+            $obj_rentcar = new SolRentCar();
+            $obj_rentcar
+                ->setCantPersona($cant_personas_rentcar)
+                ->setTipoVehiculo($tipoVehiculo_rentcar)
+                ->setRecogida($recogida_rentcar)
+                ->setEntrega($entrega_rentcar)
+                ->setFechaHasta($fechah_rentcar)
+                ->setFechaDesde($fechad_rentcar)
+                ->setComentario($comentario_rentcar)
+                ->setActivo(true);
+
+            try {
+                $em->persist($obj_rentcar);
+                $em->flush();
+            }catch (FileException $exception) {
+                $error = true;
+                return new \Exception('La petición ha retornado un error, contacte a su proveedor de software.');
+            }
+        }
+
+
         if ($error){
             $this->addFlash('error', "Datos de solicitud no adicionados");
             return new JsonResponse(['success' => false]);
